ad626d2f32f3e48873f2a55fbe827f26152a2d14
jforeman7 <31979772+jforeman7@users.noreply.github.com>
2017-11-29 14:13:15 -0800

    Update TestSim.py

--- TestSim.py
+++ TestSim.py
@@ -149,7 +149,7 @@ def main():
     s.addChannel(s.GENERAL_CHANNEL);
     #s.addChannel(s.NEIGHBOR_CHANNEL);
     #s.addChannel(s.FLOODING_CHANNEL);
-    #s.addChannel(s.ROUTING_CHANNEL);
+    s.addChannel(s.ROUTING_CHANNEL);
     s.addChannel(s.TRANSPORT_CHANNEL);
     
     s.runTime(100);
